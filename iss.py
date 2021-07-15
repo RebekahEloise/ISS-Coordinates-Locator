d351777e7644e807d55ddf7f9462475a8aa1348c
Rebekah Eloise Miller <rebekaheloisemiller@gmail.com>
2021-07-15 15:54:52 -0500

    ISS API requests

--- iss.py
+++ iss.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Rebekah Eloise Miller with help from Matt Perry'
+
+import requests
+
+
+def num_astros():
+    r = requests.get("http://api.open-notify.org/astros.json")
+    print(r.status_code)
+    print(r.json())
+
+
+def current_loc():
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    print(r.status_code)
+    print(r.json())
 
 
 def main():
-    pass
+    num_astros()
+    current_loc()
 
 
 if __name__ == '__main__':
